5198664ee5c2a9a8bcb01f5281404271ccb031c3
nishikantvijeta <128150871+nishikantvijeta@users.noreply.github.com>
2025-02-20 03:17:06 +0530

    Update axiosInstance.js

--- client/src/Helper/axiosInstance.js
+++ client/src/Helper/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://lms-server-6-9fwo.onrender.com//api/v1";
+const BASE_URL = "https://lms-server-6-9fwo.onrender.com/api/v1";
 
 const axiosInstance = axios.create();
 
