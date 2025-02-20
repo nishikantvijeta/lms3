92385e30f9ae72d8d49cdf89a9a4cc8c7b3e3211
nishikantvijeta <128150871+nishikantvijeta@users.noreply.github.com>
2025-02-20 16:12:22 +0530

    Update axiosInstance.js

--- client/src/Helper/axiosInstance.js
+++ client/src/Helper/axiosInstance.js
@@ -6,5 +6,10 @@ const axiosInstance = axios.create();
 
 axiosInstance.defaults.baseURL = BASE_URL;
 axiosInstance.defaults.withCredentials = true;
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+}, (error) => Promise.reject(error));
 
 export default axiosInstance;
